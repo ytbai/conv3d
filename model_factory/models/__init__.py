be03356eaba9e9ed6dc7accfdc00e47aa15b16a5
Yuntao Bai <ytbai@princeton.edu>
2020-07-26 16:58:01 -0700

    __ini__ for models module

---
+++ model_factory/models/__init__.py
@@ -0,0 +1,16 @@
+from model_factory.models.Baseline import *
+from model_factory.models.BaselineWide2 import *
+from model_factory.models.BaselineWide4 import *
+from model_factory.models.BaselineWide8 import *
+from model_factory.models.BaselineWide4Deep2 import *
+from model_factory.models.BaselineWide4Deep3 import *
+from model_factory.models.BaselineWide4Deep4 import *
+from model_factory.models.BaselineWide4Deep2Skip import *
+from model_factory.models.BaselineWide4Deep3Skip import *
+from model_factory.models.BaselineWide4Deep4Skip import *
+from model_factory.models.BaselineWide4Deep3SkipAutoreg import *
+from model_factory.models.BaselineAutoreg import *
+from model_factory.models.BaselineSkipAutoreg import *
+from model_factory.models.Conv2dRNNSmall import *
+from model_factory.models.Conv2dRNNLarge import *
+
